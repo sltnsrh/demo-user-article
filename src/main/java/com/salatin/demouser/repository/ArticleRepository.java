bc3f3dc2e039cdc0708e4c2780716d49fe3986e9
Serhii Salatin <lifenjoy8888@gmail.com>
2023-04-07 08:49:05 +0300

    Article- and UserRepository were created

---
+++ src/main/java/com/salatin/demouser/repository/ArticleRepository.java
@@ -0,0 +1,9 @@
+package com.salatin.demouser.repository;
+
+import com.salatin.demouser.model.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+}
