87731b6a82f953d367cd0249c3e7feebdb1d21cc
Serhii Salatin <lifenjoy8888@gmail.com>
2023-04-07 08:49:46 +0300

    User enity was updated

--- src/main/java/com/salatin/demouser/model/User.java
+++ src/main/java/com/salatin/demouser/model/User.java
@@ -21,7 +21,7 @@ import lombok.ToString;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String name;
     private short age;
     private String email;
