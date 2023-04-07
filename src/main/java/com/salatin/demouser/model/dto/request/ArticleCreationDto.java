fdebeabb082fa1e83c33ac9b373e8dd2372fdbd3
Serhii Salatin <lifenjoy8888@gmail.com>
2023-04-07 08:53:32 +0300

    ArticleCreationDto was added

---
+++ src/main/java/com/salatin/demouser/model/dto/request/ArticleCreationDto.java
@@ -0,0 +1,11 @@
+package com.salatin.demouser.model.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ArticleCreationDto {
+    private String text;
+    private String color;
+}
