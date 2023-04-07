559d2fb1d6cccf9ecf9c03ea4cabe6effbd1d641
Serhii Salatin <lifenjoy8888@gmail.com>
2023-04-07 08:28:56 +0300

    initial commit

---
+++ src/main/java/com/salatin/demouser/DemouserApplication.java
@@ -0,0 +1,13 @@
+package com.salatin.demouser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemouserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemouserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salatin/demouser/DemouserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salatin.demouser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemouserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
