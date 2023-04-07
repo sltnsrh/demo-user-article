19aa8b0de34bbeeaad1cf631aca09f75f0d2b092
Serhii Salatin <lifenjoy8888@gmail.com>
2023-04-07 09:36:13 +0300

    UserMapper was created

---
+++ src/main/java/com/salatin/demouser/service/mapper/UserMapper.java
@@ -0,0 +1,28 @@
+package com.salatin.demouser.service.mapper;
+
+import com.salatin.demouser.model.Article;
+import com.salatin.demouser.model.User;
+import com.salatin.demouser.model.dto.request.UserRegistrationRequestDto;
+import com.salatin.demouser.model.dto.response.UserRegistrationResponseDto;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    @Mapping(target = "id", ignore = true)
+    User toUser(UserRegistrationRequestDto requestDto);
+
+    @Mapping(target = "articleIds", source = "articles", qualifiedByName = "articlesToIds")
+    UserRegistrationResponseDto toDto(User user);
+
+    @Named("articlesToIds")
+    default List<Long> articlesToIds(List<Article> articles) {
+        return articles.stream()
+            .map(Article::getId)
+            .collect(Collectors.toList());
+    }
+}
