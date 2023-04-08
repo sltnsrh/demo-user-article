43832a67f41e30610b1406e46bda71678eac7693
Serhii Salatin <lifenjoy8888@gmail.com>
2023-04-08 19:12:10 +0300

    Fields validation to UserRegistrationRequestDto was added

--- src/main/java/com/salatin/demouser/exception/ApiExceptionHandler.java
+++ src/main/java/com/salatin/demouser/exception/ApiExceptionHandler.java
@@ -2,9 +2,12 @@ package com.salatin.demouser.exception;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.stream.Collectors;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -15,11 +18,32 @@ public class ApiExceptionHandler {
     @ExceptionHandler(value = {
         BadCredentialsException.class,
     })
-    public ResponseEntity<ApiExceptionObject> handleLimitedPermissionException(
+    public ResponseEntity<ApiExceptionObject> handleBadCredentialsException(
         RuntimeException e
     ) {
         HttpStatus status = HttpStatus.FORBIDDEN;
         return new ResponseEntity<>(getApiExceptionObject(e.getMessage(), status), status);
+    }
+
+    @ExceptionHandler(value = {
+        UserAlreadyExistsException.class,
+    })
+    public ResponseEntity<ApiExceptionObject> handleUserAlreadyExistsException(
+        RuntimeException e
+    ) {
+        HttpStatus status = HttpStatus.CONFLICT;
+        return new ResponseEntity<>(getApiExceptionObject(e.getMessage(), status), status);
+    }
+
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    public ResponseEntity<ApiExceptionObject> handleBadRequestException(
+        MethodArgumentNotValidException e
+    ) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        String errorMessages = e.getBindingResult().getAllErrors().stream()
+            .map(DefaultMessageSourceResolvable::getDefaultMessage)
+            .collect(Collectors.joining(", "));
+        return new ResponseEntity<>(getApiExceptionObject(errorMessages, status), status);
     }
 
     private ApiExceptionObject getApiExceptionObject(String message, HttpStatus status) {
--- src/main/java/com/salatin/demouser/model/dto/request/UserRegistrationRequestDto.java
+++ src/main/java/com/salatin/demouser/model/dto/request/UserRegistrationRequestDto.java
@@ -1,6 +1,9 @@
 package com.salatin.demouser.model.dto.request;
 
 import com.salatin.demouser.model.dto.validation.FieldsValueMatch;
+import com.salatin.demouser.model.dto.validation.ValidEmail;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,8 +14,11 @@ import lombok.Setter;
     fieldMatch = "repeatPassword",
     message = "Passwords do not match")
 public class UserRegistrationRequestDto {
+    @Size(min = 3, message = "Your name length must be at list 3 symbols")
     private String name;
+    @Min(value = 18, message = "You must be at list 18 years old")
     private short age;
+    @ValidEmail
     private String email;
     private String password;
     private String repeatPassword;
