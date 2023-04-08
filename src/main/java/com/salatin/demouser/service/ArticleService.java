e3fded240bf8a3930f0ce7daeecdc33c3dbf9fd0
Serhii Salatin <lifenjoy8888@gmail.com>
2023-04-08 17:43:29 +0300

    in ArticleService mthd save was changed, added new article to a current user

--- src/main/java/com/salatin/demouser/service/ArticleService.java
+++ src/main/java/com/salatin/demouser/service/ArticleService.java
@@ -1,7 +1,8 @@
 package com.salatin.demouser.service;
 
 import com.salatin.demouser.model.Article;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface ArticleService {
-    Article save(Article article);
+    Article save(Article article, UserDetails UserDetails);
 }
--- src/main/java/com/salatin/demouser/service/impl/ArticleServiceImpl.java
+++ src/main/java/com/salatin/demouser/service/impl/ArticleServiceImpl.java
@@ -1,18 +1,30 @@
 package com.salatin.demouser.service.impl;
 
 import com.salatin.demouser.model.Article;
+import com.salatin.demouser.model.User;
 import com.salatin.demouser.repository.ArticleRepository;
 import com.salatin.demouser.service.ArticleService;
+import com.salatin.demouser.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleServiceImpl implements ArticleService {
     private final ArticleRepository articleRepository;
+    private final UserService userService;
 
     @Override
-    public Article save(Article article) {
-        return articleRepository.save(article);
+    @Transactional
+    public Article save(Article article, UserDetails userDetails) {
+        Article savedArticle = articleRepository.save(article);
+
+        User user = userService.findByEmail(userDetails.getUsername());
+        user.getArticles().add(article);
+        userService.save(user);
+
+        return savedArticle;
     }
 }
