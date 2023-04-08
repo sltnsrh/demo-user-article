1b37a5d089efe6f8f30073c6bcde81dd1be4c985
Serhii Salatin <lifenjoy8888@gmail.com>
2023-04-08 18:42:51 +0300

    password matcher was added

--- src/main/java/com/salatin/demouser/model/dto/request/UserRegistrationRequestDto.java
+++ src/main/java/com/salatin/demouser/model/dto/request/UserRegistrationRequestDto.java
@@ -1,10 +1,15 @@
 package com.salatin.demouser.model.dto.request;
 
+import com.salatin.demouser.model.dto.validation.FieldsValueMatch;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@FieldsValueMatch(
+    field = "password",
+    fieldMatch = "repeatPassword",
+    message = "Passwords do not match")
 public class UserRegistrationRequestDto {
     private String name;
     private short age;
